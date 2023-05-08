395f6de1ff6abf21c6d71c07c19fdfffb9c6dc62
Tamanna E Kaonain <59686070+ktamanna@users.noreply.github.com>
2023-05-08 10:13:43 +0800

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,55 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 20 + 10
+30
+>>> 20.5 + 10
+30.5
+>>> 20 - 10
+10
+>>> 20 / 5
+4.0
+>>> 20 * 4
+80
+>>> 20 / 3
+6.666666666666667
+>>> 20 // 3
+6
+>>> 20 % 3
+2
+>>> 20 ** 3
+8000
+>>> 20++
+SyntaxError: invalid syntax
+>>> 20--
+SyntaxError: invalid syntax
+>>> int a = 10
+SyntaxError: invalid syntax
+>>> 20++3
+23
+>>> a1 = 10
+>>> print(a1)
+10
+>>> print(type(a1))
+<class 'int'>
+>>> umang = 10.45
+>>> print(type(umang))
+<class 'float'>
+>>> a3 = "Umang"
+>>> print(type(a3))
+<class 'str'>
+>>> a4 = 34 + 5j
+>>> print(type(a4))
+<class 'complex'>
+>>> a5 = True
+>>> a6 = False
+>>> print(type(a5))
+<class 'bool'>
+>>> a5 + a6
+1
+>>> a7 = True
+>>> a5 + a7
+2
+>>> a8 = "Umang"    # a8 is defined as string here
+>>> print("I am teaching Python")
+I am teaching Python
+>>> 
